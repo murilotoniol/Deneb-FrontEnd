885c2594727209df1e87aa86bc8d4e98e9421b26
Murilo Besson <murilo.besson@outlook.com>
2025-05-15 19:55:23 -0300

    trocando logo

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,6 @@ import './App.css'
 import Login from './pages/login/Login'
 import ReactDOM from "react-dom";
 import { Routes, Route, Link } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
 import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/Home/Home';
 import OfertaServiço from './pages/OfertaServiço/OfertaServiço'
